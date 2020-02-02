ea19e17961f2d0dad6ecb020c7b2872559e6d6e7
blocktourhost <60049299+blocktourhost@users.noreply.github.com>
2020-02-02 20:03:21 +0200

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1580666329,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg_loader-sheet0.png",
+		"images/black_fone-sheet0.png",
+		"images/botton_invite_friends-sheet0.png",
+		"images/botton_join_group-sheet0.png",
+		"images/botton_report_anerror-sheet0.png",
+		"images/button_tell_about_the_game-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js",
+		"Photon-Javascript_SDK.min.js",
+		"jquery.gritter.js",
+		"jquery.gritter.css"
+	]
+}
